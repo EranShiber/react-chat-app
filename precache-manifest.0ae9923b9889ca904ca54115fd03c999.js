0147c28d0a7fd92b0967a97230a374f2056a92f8
eran <eranshiber7@gmail.com>
2019-01-15 23:01:25 +0200

    Updates

---
+++ precache-manifest.0ae9923b9889ca904ca54115fd03c999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a219927225f2f3d518",
+    "url": "/react-chat-app/static/js/runtime~main.e2a21992.js"
+  },
+  {
+    "revision": "073d6f88ed6447734b8c",
+    "url": "/react-chat-app/static/js/main.073d6f88.chunk.js"
+  },
+  {
+    "revision": "fe7321c9b60389647c15",
+    "url": "/react-chat-app/static/js/1.fe7321c9.chunk.js"
+  },
+  {
+    "revision": "073d6f88ed6447734b8c",
+    "url": "/react-chat-app/static/css/main.84437138.chunk.css"
+  },
+  {
+    "revision": "f8f9947a5f7912c2bf07ab930cbfafda",
+    "url": "/react-chat-app/index.html"
+  }
+];
